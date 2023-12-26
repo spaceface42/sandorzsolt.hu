e6c9a72bc46d95241af875aecf2e63c3afdc9be5
Zsolt Sándor <ezsolt@gmail.com>
2023-12-26 21:02:45 +0100

    d

--- docs/rsrc/app/main.js
+++ docs/rsrc/app/main.js
@@ -19,4 +19,28 @@ function ___start() {
 
 
 
+
+
+
+
+const debounce = (fn, ms = 0) => {
+    let timeoutId;
+    return function(...args) {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => fn.apply(this, args), ms);
+    };
+};
+  
+window.addEventListener(
+    'resize',
+    debounce(() => {
+        console.log(window.innerWidth);
+        console.log(window.innerHeight);
+    }, 250)
+); // Will log the window dimensions at most every 250ms
+
+
+
+
+
 // eof
