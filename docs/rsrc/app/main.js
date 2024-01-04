b45f4cc48cb9647ac691f72f48a41fe086e8b573
Zsolt Sándor <ezsolt@gmail.com>
2024-01-04 15:55:15 +0100

    up2

--- docs/rsrc/app/main.js
+++ docs/rsrc/app/main.js
@@ -15,7 +15,35 @@ function ___start() {
 
     const partial = new FetchPartial();
     partial.fetchAll();
+
+
+
+    // smooyh scroll
+    /*
+    const lenis = new Lenis()
+    lenis.on('scroll', (e) => {
+    console.log(e)
+    })
+    function raf(time) {
+        lenis.raf(time)
+        requestAnimationFrame(raf)
+    }
+    requestAnimationFrame(raf)
+    */
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
