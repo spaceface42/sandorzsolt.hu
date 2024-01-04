9082058f3c9c5896fddc07dfb00441adfcee0a40
Zsolt Sándor <ezsolt@gmail.com>
2024-01-04 17:16:01 +0100

    aaa

--- html5-boot/docs/rsrc/app/main.js
+++ html5-boot/docs/rsrc/app/main.js
@@ -22,13 +22,13 @@ function ___start() {
         // "Producing Code" (May take some time)
         partial.fetchAll();
 
-          _scroll(); // when successful
-          myReject();  // when error
+          myResolve( console.log('resolved') ); // when successful
+          myReject( console.log('rejected') );  // when error
         });
         
         // "Consuming Code" (Must wait for a fulfilled Promise)
         myPromise.then(
-          function(value) { /* code if successful */ },
+          function(value) { /* code if successful */ __scroll() },
           function(error) { /* code if some error */ }
     );
 
@@ -46,91 +46,56 @@ function ___start() {
 
 }
 
-/*
-function _scroll() {
 
-    console.log('F: _scroll');
-
-    const sections = document.querySelectorAll('p');
-    
-    window.addEventListener("scroll", (event) => {
-      // console.log('F: Scroll event fired!');
-      setTimeout(() => {
-        // console.log('F: Waiting on scroll events...');
-
-        sections.forEach(sec => {
-
-            console.log('foreach');
-
-            let top = window.scrollY;
-            let offset = sec.offsetTop;
-            let height = sec.offsetHeight;
-
-            console.log('F: top = ', top);
-            console.log('F: top = ', offset);
-            console.log('F: top = ', height);
-
-            if (top >= offset && top < offset + height) {
-                console.log('add animate in');
-                sec.classList.add('animate-in');
-            } else {
-                console.log('remove animate in');
-                sec.classList.remove('animate-in');
-            }
-        })
-
-        // output.innerHTML = "Waiting on scroll events...";
-
-      }, 1000);
-    });
-
-
-}*/
 
 function _scroll() {
 
-console.log('_scroll')
+    var target = document.getElementsByTagName("div")[0]
 
+    function callback(e, o) {
+        console.log('callback e ')
+        target.style.opacity = e[0].intersectionRatio
+    }
 
+    var options = {
+    root: null,
+    threshold: [0, 0.1, 0.2, 0.3, 0.5, 0.7, 0.8, 0.9, 1, 1, 1]
+    }
 
-
-// var target = document.getElementsByTagName("blockquote")[0]
-
-var target = document.getElementsByTagName("blockquote")[0]
-
-
-function callback(e, o) {
-    console.log('callback e ')
-   target.style.opacity = e[0].intersectionRatio
-
-}
-
-var options = {
-   root: null,
-   threshold: [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
-}
-
-var io = new IntersectionObserver(callback, options)
-io.observe(target)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    var io = new IntersectionObserver(callback, options)
+    io.observe(target)
 
 }
 
 
+function __scroll() {
+
+
+    const element = document.getElementById('main');
+
+    function lastIntersectionObserver(element) {
+        let lastIntersection;
+        return new Promise((resolve, reject) => {
+            lastIntersection = new IntersectionObserver((entries) => {
+            let entry = entries[0];
+            if (!entry.isIntersecting) return;
+                console.log('Element is visible ', entry.target);
+                resolve(true);
+            });
+            lastIntersection.observe(element);
+        });
+    }
+
+    let lastElement = document.querySelector('.card-element:last-child');
+
+    startObservingElement(lastElement);
+
+    function startObservingElement(element) {
+        lastIntersectionObserver(element).then((result) => console.log({ result }));
+    }
+
+
+}
 
 
 
@@ -139,6 +104,17 @@ io.observe(target)
 
 
 
+
+
+
+
+
+
+
+
+
+
+/* DEBOUNCE */
 
 
 const debounce = (fn, ms = 0) => {
